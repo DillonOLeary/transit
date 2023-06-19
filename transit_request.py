27b1c30f57520e6e03c285ba20fa0ff3f89c988d
Dillon O'Leary <doleary@wisc.edu>
2023-06-18 18:45:19 -0700

    Add predicted arrival time

--- transit_request.py
+++ transit_request.py
@@ -9,13 +9,19 @@ api_key = os.environ["ONEBUSAWAY_API_KEY"]
 r = requests.get(f"http://api.pugetsound.onebusaway.org/api/where/arrivals-and-departures-for-stop/1_990003.json?key={api_key}")
 r = r.json()
 
-keys_of_interest = ["scheduledArrivalTime", "predictedArrivalTime"]
+keys_of_interest = ["routeShortName", "tripHeadsign", "stopId", "scheduledArrivalTime", "predictedArrivalTime"]
 
 filtered_response = []
 
 for elem in r["data"]["entry"]["arrivalsAndDepartures"]:
     filtered_elem = {key: elem[key] for key in keys_of_interest}
+
     filtered_elem["scheduledArrivalTime"] = datetime.datetime.fromtimestamp(filtered_elem["scheduledArrivalTime"]/1000.0).ctime()
+    if filtered_elem["predictedArrivalTime"] != 0:
+        filtered_elem["predictedArrivalTime"] = datetime.datetime.fromtimestamp(filtered_elem["predictedArrivalTime"]/1000.0).ctime()
+    else:
+        filtered_elem["predictedArrivalTime"] = "no data"
     filtered_response.append({key: filtered_elem[key] for key in keys_of_interest})
 
+print("Transit Data Filtered Results")
 print(json.dumps(filtered_response, indent=4, sort_keys=True, default=str))
